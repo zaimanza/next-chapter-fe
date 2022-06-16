6bd1ff4828313171a4509529882bc4412345f96f
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-06-16 16:15:28 +0800

    button

--- src/pages/welcome.page.js
+++ src/pages/welcome.page.js
@@ -1,11 +1,11 @@
+import { HiDownload } from 'react-icons/hi';
 
-
-function WelcomePage() {
+const WelcomePage = () => {
   return (
-    <div>
+    <div className="bg-black">
       welcome page
-      <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
-        <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" /></svg>
+      <button className="bg-white bg-opacity-70 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+        <HiDownload className="fill-current w-4 h-4 mr-2" />
         <span>Download</span>
       </button>
     </div>
