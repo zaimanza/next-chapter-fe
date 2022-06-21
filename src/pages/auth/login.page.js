aab79ea5b67e490da8d4651530aed409d9a43d57
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-06-21 23:39:31 +0800

    login

--- src/pages/auth/login.page.js
+++ src/pages/auth/login.page.js
@@ -23,8 +23,6 @@ export default function LoginPage({ setAuthMode }) {
     const [getEmailError, setEmailError] = useState()
     const [getPasswordError, setPasswordError] = useState()
     const [getToastConfig, setToastConfig] = useState()
-    const email_ref = useRef()
-    const password_ref = useRef()
 
     useEffect(() => {
         if (run_uno.current === false) {
@@ -32,35 +30,23 @@ export default function LoginPage({ setAuthMode }) {
             console.log("pel")
             setEmailValue(authProvider.email)
             setPasswordValue(authProvider.password)
-            window.addEventListener('keydown', e => { })
-            window.addEventListener('keydown', e => {
-                if (e.key === 'Enter') {
-                    console.log('You pressed Enter')
-                    console.log(email_ref.current)
-                    handleSubmit()
-                }
-            })
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
-    useEffect(() => {
-        email_ref.current = getEmailValue
-        password_ref.current = getPasswordValue
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [getEmailValue, getPasswordValue])
 
-    const handleSubmit = async () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault()
         //regex email
-        const emailRes = regexEmail(email_ref.current)
+        const emailRes = regexEmail(getEmailValue)
         if (emailRes) setEmailError(emailRes)
         //regex password
-        const passwordRes = regexPassword(password_ref.current)
+        const passwordRes = regexPassword(getPasswordValue)
         if (passwordRes?.message) setPasswordError(passwordRes.message)
 
         if (!emailRes && !passwordRes?.message) {
             const result = await _useAuthModule.peopleLogin({
-                email: email_ref.current,
-                password: password_ref.current
+                email: getEmailValue,
+                password: getPasswordValue
             })
 
             if (result?.error || !result) {
@@ -99,110 +85,112 @@ export default function LoginPage({ setAuthMode }) {
 
     return (
         <div className="container mx-auto px-4 h-full">
-            <div className="flex content-center items-center justify-center h-full">
-                <div className="w-full lg:w-4/12 px-4">
-                    <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg border-0 bg-white">
-                        <div className="rounded-t mb-0 px-6 py-6">
-                            <div className="text-center">
-                                <div className="text-center mb-3 font-bold ">
-                                    Log into your <label className="text-xl">nextChapter</label>
+            <form onSubmit={handleSubmit}>
+                <div className="flex content-center items-center justify-center h-full">
+                    <div className="w-full lg:w-4/12 px-4">
+                        <div className="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg border-0 bg-white">
+                            <div className="rounded-t mb-0 px-6 py-6">
+                                <div className="text-center">
+                                    <div className="text-center mb-3 font-bold ">
+                                        Log into your <label className="text-xl">nextChapter</label>
+                                    </div>
+                                </div>
+                                <div className="text-center">
+                                    <div className="text-center mb-3 font-normal">
+                                        <small>Continue your journey.</small>
+                                    </div>
+                                </div>
+                                {timerCountdown === 0 ? null : <StaticToast
+                                    config={getToastConfig ?? {
+                                        message: "Website is unavailable. Please try again later.",
+                                        mode: "error"
+                                    }}
+                                />}
+                                <div className="relative w-full mb-3">
+                                    <label
+                                        className="block uppercase text-xs font-bold mb-2"
+                                        htmlFor="grid-password"
+                                    >
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        className={`${getEmailError ? "ring ring-red-500 placeholder-red-300 text-red-600" : "focus:ring placeholder-gray-300 text-gray-600"} focus:outline-none border-0 px-3 py-3 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150`}
+                                        placeholder="Email"
+                                        value={getEmailValue}
+                                        onChange={(event) => {
+                                            setEmailError("")
+                                            const onChangeValue = event.target.value
+                                            setEmailValue(onChangeValue)
+                                            dispatch(
+                                                authSetEmailReducer({
+                                                    email: onChangeValue,
+                                                })
+                                            )
+                                        }}
+                                    />
+                                    {getEmailError ? (<p className="text-sm text-red-400 mt-1">{getEmailError}</p>) : null}
+                                </div>
+
+                                <div className="relative w-full mb-3">
+                                    <label
+                                        className="block uppercase text-xs font-bold mb-2"
+                                        htmlFor="grid-password"
+                                    >
+                                        Password
+                                    </label>
+                                    <input
+                                        type="password"
+                                        className={`${getPasswordError ? "ring ring-red-500 placeholder-red-300 text-red-600" : "focus:ring placeholder-gray-300 text-gray-600"} focus:outline-none border-0 px-3 py-3 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150`}
+                                        placeholder="Password"
+                                        value={getPasswordValue}
+                                        onChange={(event) => {
+                                            setPasswordError("")
+                                            const onChangeValue = event.target.value
+                                            setPasswordValue(onChangeValue)
+                                            dispatch(
+                                                authSetPasswordReducer({
+                                                    password: onChangeValue,
+                                                })
+                                            );
+                                        }}
+                                    />
+                                    {getPasswordError ? (<p className="text-sm text-red-400 mt-1">{getPasswordError}</p>) : null}
+                                </div>
+
+                                <div className="mb-11">
+                                </div>
+
+                                <div className="text-center mt-6">
+                                    <button
+                                        className="bg-gray-800 text-white active:bg-gray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
+                                        type="submit"
+                                        onClick={handleSubmit}
+                                    >
+                                        Sign In
+                                    </button>
                                 </div>
                             </div>
-                            <div className="text-center">
-                                <div className="text-center mb-3 font-normal">
-                                    <small>Continue your journey.</small>
+                        </div>
+                        <div className="flex flex-wrap mt-6 relative">
+                            <div className="w-1/2">
+                                <div onClick={() => {
+                                    setAuthMode("forgot-password")
+                                }} className="mr-auto w-fit hover:underline hover:text-blue-600">
+                                    <small>Forgot password?</small>
                                 </div>
                             </div>
-                            {timerCountdown === 0 ? null : <StaticToast
-                                config={getToastConfig ?? {
-                                    message: "Website is unavailable. Please try again later.",
-                                    mode: "error"
-                                }}
-                            />}
-                            <div className="relative w-full mb-3">
-                                <label
-                                    className="block uppercase text-xs font-bold mb-2"
-                                    htmlFor="grid-password"
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    type="email"
-                                    className={`${getEmailError ? "ring ring-red-500 placeholder-red-300 text-red-600" : "focus:ring placeholder-gray-300 text-gray-600"} focus:outline-none border-0 px-3 py-3 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150`}
-                                    placeholder="Email"
-                                    value={getEmailValue}
-                                    onChange={(event) => {
-                                        setEmailError("")
-                                        const onChangeValue = event.target.value
-                                        setEmailValue(onChangeValue)
-                                        dispatch(
-                                            authSetEmailReducer({
-                                                email: onChangeValue,
-                                            })
-                                        )
-                                    }}
-                                />
-                                {getEmailError ? (<p className="text-sm text-red-400 mt-1">{getEmailError}</p>) : null}
-                            </div>
-
-                            <div className="relative w-full mb-3">
-                                <label
-                                    className="block uppercase text-xs font-bold mb-2"
-                                    htmlFor="grid-password"
-                                >
-                                    Password
-                                </label>
-                                <input
-                                    type="password"
-                                    className={`${getPasswordError ? "ring ring-red-500 placeholder-red-300 text-red-600" : "focus:ring placeholder-gray-300 text-gray-600"} focus:outline-none border-0 px-3 py-3 bg-white rounded text-sm shadow w-full ease-linear transition-all duration-150`}
-                                    placeholder="Password"
-                                    value={getPasswordValue}
-                                    onChange={(event) => {
-                                        setPasswordError("")
-                                        const onChangeValue = event.target.value
-                                        setPasswordValue(onChangeValue)
-                                        dispatch(
-                                            authSetPasswordReducer({
-                                                password: onChangeValue,
-                                            })
-                                        );
-                                    }}
-                                />
-                                {getPasswordError ? (<p className="text-sm text-red-400 mt-1">{getPasswordError}</p>) : null}
-                            </div>
-
-                            <div className="mb-11">
-                            </div>
-
-                            <div className="text-center mt-6">
-                                <button
-                                    className="bg-gray-800 text-white active:bg-gray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 w-full ease-linear transition-all duration-150"
-                                    type="button"
-                                    onClick={handleSubmit}
-                                >
-                                    Sign In
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="flex flex-wrap mt-6 relative">
-                        <div className="w-1/2">
-                            <div onClick={() => {
-                                setAuthMode("forgot-password")
-                            }} className="mr-auto w-fit hover:underline hover:text-blue-600">
-                                <small>Forgot password?</small>
-                            </div>
-                        </div>
-                        <div className="w-1/2 text-right">
-                            <div onClick={() => {
-                                setAuthMode("register")
-                            }} className="ml-auto w-fit hover:underline hover:text-blue-600">
-                                <small>Create new account</small>
+                            <div className="w-1/2 text-right">
+                                <div onClick={() => {
+                                    setAuthMode("register")
+                                }} className="ml-auto w-fit hover:underline hover:text-blue-600">
+                                    <small>Create new account</small>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     );
 }
