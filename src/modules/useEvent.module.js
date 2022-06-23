919c278e663cac28e9c9d0a895966166174b1a4e
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-06-23 17:00:26 +0800

    find_all_general_by_owner_id

--- src/modules/useEvent.module.js
+++ src/modules/useEvent.module.js
@@ -30,8 +30,39 @@ const useEventModule = () => {
         return result?.data
     }
 
+    const findAllGeneralByOwnerId = async ({ owner_id }) => {
+
+        const result = await axios?.post(
+            `http://localhost:3001/event/find_all_general_by_owner_id`,
+            {
+                owner_id: owner_id
+            }
+        )
+            .catch(function (error) {
+                if (error?.response?.data) {
+                    return {
+                        error: error?.response?.data ?? "Website is currently offline."
+                    }
+                } else {
+                    return
+                }
+            })
+
+        if (!result?.data) {
+            if (result?.error) {
+                return {
+                    error: result?.error ?? "Website is currently offline."
+                }
+            } else {
+                return
+            }
+        }
+        return result?.data
+    }
+
     return {
-        createEvent
+        createEvent,
+        findAllGeneralByOwnerId
     }
 }
 
--- src/pages/events/events.page.js
+++ src/pages/events/events.page.js
@@ -1,23 +1,60 @@
 
 
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { HiDownload } from 'react-icons/hi';
 import { useDispatch, useSelector } from "react-redux"
 import { useNavigate } from 'react-router-dom';
+import useEventModule from '../../modules/useEvent.module';
 import { peopleLogoutReducer } from '../../providers/people.provider';
 
 const EventsPage = () => {
     const navigate = useNavigate()
+    const _useEventModule = useEventModule()
     const dispatch = useDispatch()
     const run_uno = useRef(false)
 
     const peopleProvider = useSelector((state) => state.people.value)
+
+    // eslint-disable-next-line no-unused-vars
+    const [getToastConfig, setToastConfig] = useState()
 
     useEffect(() => {
         if (run_uno.current === false) {
             run_uno.current = true
             if (peopleProvider.access_token !== "") {
 
+                const initFunctionCall = async () => {
+
+                    // findallByOwnerId
+                    const result = await _useEventModule.findAllGeneralByOwnerId({
+                        owner_id: peopleProvider?.uid
+                    })
+
+                    if (result?.error || !result) {
+                        if (result?.error?.error) {
+
+                            setToastConfig({
+                                message: "Website is unavailable. Please try again later.",
+                                mode: "error"
+                            })
+                        } else if (result?.error) {
+                            setToastConfig({
+                                message: result?.error ?? "Website is unavailable. Please try again later.",
+                                mode: "error"
+                            })
+                        }
+                    } else {
+                        if (result.length !== 0) {
+                            // if exist view
+                        } else {
+                            // else go to create_event
+                            navigate("/create_event")
+                        }
+                    }
+
+                    // if error view problem with system
+                }
+                initFunctionCall()
             } else {
                 navigate("/auth");
             }
