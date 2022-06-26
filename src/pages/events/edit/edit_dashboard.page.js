f70c9213a5e14e39f55cfb0d9395c8974e489b81
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-06-27 00:46:21 +0800

    edit filter

--- src/pages/events/edit/edit_dashboard.page.js
+++ src/pages/events/edit/edit_dashboard.page.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EditDashboardPage = () => {
+    const navigate = useNavigate()
+    const run_uno = useRef(false)
+    const { nc_wedding_id } = useParams()
+
+    const peopleProvider = useSelector((state) => state.people.value)
+
+    useEffect(() => {
+        if (run_uno.current === false) {
+            run_uno.current = true
+            if (peopleProvider.access_token === "") {
+                navigate(`/${nc_wedding_id}`)
+            }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
     return (
         <div className="">
             <div className='bottom-0 right-0 fixed mr-4 mb-4'>
--- src/pages/events/events.page.js
+++ src/pages/events/events.page.js
@@ -195,7 +195,7 @@ const EventsPage = () => {
                         }}
                     /> : null}
                 {getCardList.length !== 0 ?
-                    <div className="grid grid-flow-row grid-cols-1 md:grid-cols-3 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
+                    <div className="grid grid-flow-row grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-10">
                         {getCardList.map((oneEvent, index) =>
                             <div
                                 key={index}
