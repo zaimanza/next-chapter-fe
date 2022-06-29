4f759fe0547ea265de199af23800131b101c3636
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-06-30 02:30:16 +0800

    card detail page

--- src/pages/events/edit/card_detail.page.js
+++ src/pages/events/edit/card_detail.page.js
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const CardDetailPage = () => {
+    const navigate = useNavigate()
+    const run_uno = useRef(false)
+    const { nc_wedding_id } = useParams()
+
+    const peopleProvider = useSelector((state) => state.people.value)
+
+    useEffect(() => {
+        if (run_uno.current === false) {
+            run_uno.current = true
+            if (peopleProvider.access_token === "") {
+                navigate(`/${nc_wedding_id}`)
+            }
+        }
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
     return (
         <div>card_detail.page</div>
     )
