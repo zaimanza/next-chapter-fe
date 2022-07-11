6d8cfed8935ce3fdb5557dae626c364b0ef5a71d
MANZA <36519974+zaimanza@users.noreply.github.com>
2022-07-11 18:51:45 +0800

    no img

--- src/components/wedding_card_template/Silk.template.js
+++ src/components/wedding_card_template/Silk.template.js
@@ -78,28 +78,37 @@ const SilkTemplate = ({
 
 
     return (
-        <div className="text-[1.7vh] flex items-start bg-white">
-            <div className='text-black sticky top-0 hidden sm:block h-[100vh] w-full'>
+        <div className={`text-[1.7vh] flex items-start bg-white ${getCurrentImg !== "" ? `` : `shadow-[0_6px_0px_0px] shadow-black w-full items-center justify-center`}`}>
 
-                <div className="absolute w-full ">
-                    <div className="relative w-full z-50">
-                        <HeaderComponent />
+            {
+                getCurrentImg !== "" ? (
+                    <div className='text-black sticky top-0 hidden sm:block h-[100vh] w-full'>
+                        <div className="absolute w-full ">
+                            <div className="relative w-full z-50">
+                                <HeaderComponent />
+                            </div>
+                        </div>
+                        <LeftComponent />
                     </div>
-                </div>
-
-                <LeftComponent />
-            </div>
-            <div className='w-full sm:min-w-[530px] sm:max-w-[580px]'>
+                ) : null
+            }
+            <div className={`w-full sm:min-w-[530px] sm:max-w-[580px] ${getCurrentImg !== "" ?
+                `` :
+                `w-full `}`}>
                 <div id="body-navbar" className="fixed z-50 flex w-full sm:min-w-[530px] sm:max-w-[580px]">
-                    <div className="relative w-full block sm:hidden">
+                    <div className={`relative w-full block ${getCurrentImg !== "" ? `sm:hidden` : ``}`}>
                         <HeaderComponent />
                     </div>
                 </div>
 
                 <div id="body" className="">
-                    <div className="block sm:hidden">
-                        <LeftComponent />
-                    </div>
+                    {
+                        getCurrentImg !== "" ? (
+                            <div className="block sm:hidden">
+                                <LeftComponent />
+                            </div>
+                        ) : null
+                    }
                     {
                         getDisplayData?.data?.map((currentValue, index) => {
 
@@ -116,9 +125,8 @@ const SilkTemplate = ({
                                                 <img
                                                     alt="ss"
                                                     className=" object-cover h-[100vh] min-h-[50rem] sm:h-[100vh] sm:min-h-[100vh] w-full"
-                                                    src="https://aliveinvite.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbHhYIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--813fb1481ee2552eda4282e915d2780dc8d469ea/Syazwani-Main.png">
+                                                    src="https://aliveinvite.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBbHhYIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--813fb1481ee2552eda4282e915d2780dc8d469ea/Syazwani-Main.png" />
 
-                                                </img>
                                             </div>
                                             <div className="absolute h-[100vh] min-h-[50rem] sm:h-[100vh] sm:min-h-[100vh] w-full flex justify-center items-center ">
                                                 <div>{getDisplayData?.data[index]?.item_name}</div>
